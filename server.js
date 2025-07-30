c90ed9af4c2e6f778e7738cb448456af973c28cb
vyas992 <vyas70125@gmail.com>
2025-07-30 14:46:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Pokemon.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Handle form submission
